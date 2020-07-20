e3347990325ad0aa5f046f2e0ddb9486f7b03ada
atish <atish@cratconnections.com>
2020-07-20 22:24:48 +0530

    change app.js and remove http css in index.html

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const express = require("express");
 const path = require("path");
-var fs = require("fs");
-var https = require("https");
+
 const app = express();
+
+// Certificate
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/keypoints.in/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/keypoints.in/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/keypoints.in/chain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-https
-  .createServer(
-    {
-      key: fs.readFileSync(path.join(__dirname, "server.key")),
-      cert: fs.readFileSync(path.join(__dirname, "server.cert"))
-    },
-    app
-  )
-  .listen(80);
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
+
+httpsServer.listen(443, () => {
+  console.log("HTTPS Server running on port 443");
+});

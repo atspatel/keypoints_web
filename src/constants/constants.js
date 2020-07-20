6d785fbb23a8423d739f23aaa277cba4ebcf9231
atish <atish@cratconnections.com>
2020-07-20 23:20:08 +0530

    path join

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,10 +2,8 @@ import path from "path";
 
 // export const BASE_DIR = ".";
 export const BASE_DIR = "https://storage.googleapis.com/kp_videos/realme_demo";
-export const video_url = path.join(
-  BASE_DIR,
-  "media/realme_demo_1_compressed.mp4"
-);
+export const video_url =
+  "https://storage.googleapis.com/kp_videos/realme_demo/media/realme_demo_1_compressed.mp4";
 
 export const url_thumbnail = path.join(BASE_DIR, "media/realme_thumbnail.png");
 

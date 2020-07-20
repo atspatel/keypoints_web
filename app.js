deae15bdf4c4873f15865ad77a85cdef2f27088f
atish <atish@cratconnections.com>
2020-07-20 18:44:35 +0530

    test ssl

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(80);
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert")
+    },
+    app
+  )
+  .listen(80);

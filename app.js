e784636686273cf33a1d88b3985b47b35467fd43
atish <atish@cratconnections.com>
2020-07-20 18:48:10 +0530

    test ssl

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+var fs = require("fs");
+var https = require("https");
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function(req, res) {

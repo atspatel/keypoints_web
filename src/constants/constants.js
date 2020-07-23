caa0cef82f592d3e6f5995dbda2ea030bc6284fe
atish <atish@cratconnections.com>
2020-07-23 19:25:39 +0530

    change path to realme logo

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,5 +3,5 @@ export const BASE_DIR = "https://storage.googleapis.com/kp_videos/media";
 export const kp_url = "https://keypoints.in/";
 
 // TODO:: shift this to realme constants and send them as props to SpecificationPopUp
-export const realme_logo = `${BASE_DIR}/realme_demo/media/realme_logo_transparent.png`;
-export const gadgets360_logo = `${BASE_DIR}/realme_demo/media/gadgets360_logo.png`;
+export const realme_logo = `${BASE_DIR}/realme_demo/realme_logo_transparent.png`;
+export const gadgets360_logo = `${BASE_DIR}/realme_demo/gadgets360_logo.png`;

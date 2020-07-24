76a41653fd0123a6ac2fb7fc6c1d0d9d6a8408ad
atish <atish@cratconnections.com>
2020-07-25 02:08:22 +0530

    hemet header in server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import ReactDOMServer from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 
 import * as constants from "../src/constants/constants";
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 
 import App from "../src/App";
 
@@ -20,19 +20,19 @@ app.get("/*", function(req, res) {
       <App />
     </StaticRouter>
   );
-  const indexFile = path.resolve("./build/index.html");
-  fs.readFile(indexFile, "utf8", (err, data) => {
-    if (err) {
-      console.error("Something went wrong:", err);
-      return res.status(500).send("Oops, better luck next time!");
-    }
+  // const indexFile = path.resolve("./build/index.html");
+  // fs.readFile(indexFile, "utf8", (err, data) => {
+  //   if (err) {
+  //     console.error("Something went wrong:", err);
+  //     return res.status(500).send("Oops, better luck next time!");
+  //   }
 
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    );
-  });
-  // const helmet = Helmet.renderStatic();
-  // return res.send(formatHTML(app, helmet));
+  //   return res.send(
+  //     data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+  //   );
+  // });
+  const helmet = Helmet.renderStatic();
+  return res.send(formatHTML(app, helmet));
 });
 
 // Starting both http servers
@@ -43,7 +43,6 @@ httpServer.listen(80, () => {
   console.log("HTTP Server running on port 80");
 });
 
-// console.log(load_https, "--------");
 if (constants.load_https) {
   // Certificate & Starting both https servers
   const https = require("https");
@@ -70,19 +69,19 @@ if (constants.load_https) {
   });
 }
 
-// function formatHTML(appStr, helmet) {
-//   return `<!doctype html>
-//       <html lang="en" ${helmet.htmlAttributes.toString()}>
-//         <head>
-//           <base href="/" />
-//           ${helmet.title.toString()}
-//           ${helmet.meta.toString()}
-//         </head>
-//         <body ${helmet.bodyAttributes.toString()}>
-//           <div id="root">
-//             ${appStr}
-//           </div>
-//           <script src="./bundle.js"></script>
-//         </body>
-//       </html>`;
-// }
+function formatHTML(appStr, helmet) {
+  return `<!doctype html>
+      <html lang="en" ${helmet.htmlAttributes.toString()}>
+        <head>
+          <base href="/" />
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+        </head>
+        <body ${helmet.bodyAttributes.toString()}>
+          <div id="root">
+            ${appStr}
+          </div>
+          <script src="./bundle.js"></script>
+        </body>
+      </html>`;
+}

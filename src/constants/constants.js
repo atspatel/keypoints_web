f2c402da58cf8f820f45f6d9a51b08c9f42462dd
atish <atish@cratconnections.com>
2020-07-24 20:21:51 +0530

    change base_dir

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const BASE_DIR = "./media";
-// export const BASE_DIR = "https://storage.googleapis.com/kp_videos/media";
+// export const BASE_DIR = "./media";
+export const BASE_DIR = "https://storage.googleapis.com/kp_videos/media";
 export const kp_url = "https://keypoints.in/";
 
 // TODO:: shift this to realme constants and send them as props to SpecificationPopUp

469c6b43e1d5a6aa59cee776e4841be276531e5e
atish <atish@cratconnections.com>
2020-07-28 00:33:14 +0530

    oximeter rect

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -3,6 +3,9 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import RealmeVideo from "./VideoPages/RealmeVideo";
 import OximeterVideo, { OximeterEmbedVideo } from "./VideoPages/OximeterVideo";
+import OximeterRectVideo, {
+  OximeterRectEmbedVideo
+} from "./VideoPages/OximeterRectVideo";
 
 class AppRoute extends Component {
   render() {
@@ -12,6 +15,11 @@ class AppRoute extends Component {
         <Route path="/realme" render={() => <RealmeVideo />} />
         <Route path="/oximeter" render={() => <OximeterVideo />} />
         <Route path="/embed_oximeter" render={() => <OximeterEmbedVideo />} />
+        <Route path="/oximeter_rect" render={() => <OximeterRectVideo />} />
+        <Route
+          path="/embed_oximeter_rect"
+          render={() => <OximeterRectEmbedVideo />}
+        />
         <Route render={() => <Redirect to="/sample_video" />} />
       </Switch>
     );

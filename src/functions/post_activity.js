177e993128cad48eeb8f354d1d9531b351a65f46
atish <atish@cratconnections.com>
2020-09-03 11:27:58 +0530

    primary duration

--- src/functions/post_activity.js
+++ src/functions/post_activity.js
@@ -17,14 +17,20 @@ export async function post_activity(activity, video_id, button_id, session_id) {
   return output;
 }
 
-export async function post_duration(video_id, duration, session_id) {
+export async function post_duration(
+  video_id,
+  duration,
+  session_id,
+  duration_1
+) {
   const api_url = `${config.host}/activity_ops/post_duration/`;
   var output = null;
 
   const data = {
     video_id: video_id,
     session_id: session_id,
-    duration: duration.toString()
+    duration: duration.toString(),
+    duration_1: duration_1.toString()
   };
   await axios.post(api_url, data).then(response => {
     output = response.data;

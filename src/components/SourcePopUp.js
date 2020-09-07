eef41fba8e6b9e38e7bb248a44ae9c24242c091e
atish <atish@cratconnections.com>
2020-09-07 14:43:04 +0530

    navigator share test

--- src/components/SourcePopUp.js
+++ src/components/SourcePopUp.js
@@ -18,7 +18,23 @@ class SourceCard extends Component {
           margin: 5,
           borderBottom: "1px solid black"
         }}
-        onClick={e => openUrlInTab(card_info.url)}
+        // onClick={e => openUrlInTab(card_info.url)}
+        onClick={e => {
+          window.alert(navigator.share);
+          if (navigator.share) {
+            navigator
+              .share({
+                title: "WebShare API Demo",
+                url: "https://codepen.io/ayoisaiah/pen/YbNazJ"
+              })
+              .then(() => {
+                console.log("Thanks for sharing!");
+              })
+              .catch(console.error);
+          } else {
+            // fallback
+          }
+        }}
       >
         <p style={{ margin: 0, padding: 0 }} className="text">
           {card_info.url}
@@ -36,7 +52,8 @@ class SourcePopUp extends Component {
           height: "100%",
           width: "100%",
           padding: "5%",
-          position: "absolute"
+          position: "absolute",
+          overflowY: "scroll"
         }}
       >
         <div

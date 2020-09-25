80b69c6576ccef3933e65ffaaa9646d32232d155
Aatish Patel <atish@cratconnections.com>
2020-09-25 15:01:21 +0530

    added image

--- src/components/WebShareComponent.js
+++ src/components/WebShareComponent.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
+import imageDataURI from "image-data-uri";
 
 class WebShareComponent extends Component {
   onClick = () => {
     if (navigator.share) {
-      navigator
-        .share({
-          title: "web.dev",
-          text: "Check out web.dev.",
-          url: "https://web.dev/"
-        })
-        .then(() => console.log("Successful share"))
-        .catch(error => console.log("Error sharing", error));
+      imageDataURI
+        .encodeFromURL(
+          "https://storage.googleapis.com/kp_videos/ProfilePics/IMG-20200701-WA0013.jpg"
+        )
+        .then(res =>
+          navigator
+            .share({
+              title: "web.dev",
+              text: "Check out web.dev.",
+              url: "https://web.dev/",
+              files: [res]
+            })
+            .then(() => console.log("Successful share"))
+            .catch(error => console.log("Error sharing", error))
+        );
     } else {
       console.log("not supported");
     }

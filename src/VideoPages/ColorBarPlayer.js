d851e2f2c60db99eb8cc6d9d8518265393e8b37c
Aatish Patel <atish@cratconnections.com>
2020-12-02 12:43:53 +0530

    live p_id for colorbar

--- src/VideoPages/ColorBarPlayer.js
+++ src/VideoPages/ColorBarPlayer.js
@@ -7,7 +7,8 @@ const { innerHeight, innerWidth } = window;
 class ColorBarPlayer extends Component {
   render() {
     const { location } = this.props;
-    let p_id = "5afc9113-d9cf-4164-a185-1f4456a1731d";
+    // let p_id = "5afc9113-d9cf-4164-a185-1f4456a1731d"; //localhost
+    let p_id = "80674f5e-3465-45a2-9447-3d8480452a57"; // live
     if (location && location.search) {
       const qParams = queryString.parse(location.search);
       p_id = qParams.p_id ? qParams.p_id : p_id;
